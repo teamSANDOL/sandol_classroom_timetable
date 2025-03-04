ef09db5b97ace7d971d86c382f5877c29cca3fec
k1star <187358260+k1star@users.noreply.github.com>
2025-03-04 17:14:05 +0900

    feat: CookieManager 추가

--- utils/request.js
+++ utils/request.js
@@ -2,6 +2,7 @@ const {URL}=require('url');
 const http=require('http');
 const https=require('https');
 const zlib=require('zlib');
+const fs=require('fs');
 
 function request(method,url,headers,body)
 {
@@ -42,5 +43,56 @@ function request(method,url,headers,body)
         req.on('error',reject);
         req.end(body);
     });
+}
+
+function CookieManager(){
+    this.cookies=new Map();
+    this.write=function(path){
+        fs.writeFileSync(path,JSON.stringify(Array.from(this.cookies)));
+    };
+    this.read=function(path){
+        if(fs.existsSync(path))this.cookies=new Map(JSON.parse(fs.readFileSync(path).toString()));
+    };
+    this.clean=function(){
+        for (let [key, value] of this.cookies) {
+            if(value.expireTime-Date.now()<0){
+                this.cookies.delete(key);
+            }
+        }
+    };
+    this.cookieString=function(hostname){
+        this.clean();
+        return Array.from(this.cookies).filter(x=>{
+            const domain=x.properties.domain;
+            if(domain===undefined)return false;
+            if(hostname==domain)return true;
+            if(domain[0]=='.'){
+                if(hostname.includes(domain)){
+                    return true;
+                }else if('.'+hostname==domain){
+                    return true;
+                }
+            }
+            return false;
+        }).map(x=>x[0]+'='+encodeURIComponent(x[1].value)).join('; ');
+    };
+    this.set=function(str){
+        const [[name,value],...properties]=str.split('; ').map(x=>Array.from(new URLSearchParams(x))[0]);
+        const o={};
+        properties.forEach(x=>o[x[0]]=x[1]);
+        let expireTime=Infinity;
+        if(o['expires']!==undefined)expireTime=Math.min(expireTime,new Date(o['expires']).getTime());
+        if(o['Max-Age']!==undefined)expireTime=Math.min(expireTime,new Date(Date.now()+Number(o['Max-Age'])*1000).getTime());
+        this.cookies.set(name,{
+            value:value,
+            properties:o,
+            expireTime:expireTime,
+        });
+        this.clean();
+    };
+    this.setByRes=function(res){
+        res.headers?.['set-cookie']?.forEach(x=>this.set(x));
+    };
+    return this;
 }
 
